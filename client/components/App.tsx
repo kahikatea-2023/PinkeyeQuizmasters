44c826638fa08a159e1de5c61a2dbd7779d48595
Rigelle Cruz <rigellecruz@gmail.com>
2023-06-16 10:56:57 +1200

    GameOver component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -15,3 +15,34 @@ function App() {
 }
 
 export default App
+
+// example of App.tsx so that the GameOver component works
+
+// import React, { useState } from 'react';
+// import GameOver from './GameOver';
+// import Questions from './Questions';
+
+// function App() {
+//   const [isGameOver, setIsGameOver] = useState(false);
+
+//   const handleGameOver = () => {
+//     setIsGameOver(true);
+//   };
+
+//   const handleRestartGame = () => {                          <----------- // updates the states
+//     setIsGameOver(false);
+//   };
+
+//   return (
+//     <>
+//       <h1>True or False?</h1>
+//       {isGameOver ? (
+//         <GameOver restartGame={handleRestartGame} />                           <-------- // this allows the pages to switch over when the Game is Over//
+//       ) : (
+//         <Questions restartGame={handleRestartGame} />
+//       )}
+//     </>
+//   );
+// }
+
+// export default App;
